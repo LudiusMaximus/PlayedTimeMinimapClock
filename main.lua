854a03e848e2bb3b3c67319b0ab992ed05333a1f
LudiusMaximus <4678285+LudiusMaximus@users.noreply.github.com>
2023-07-19 08:57:40 +0200

    Showing tooltip immediately at mouse over (unlike the standard UI).

--- main.lua
+++ main.lua
@@ -7,7 +7,14 @@ timeManagerFrame:SetScript("OnEvent", function(self, event, ...)
 
   local name = ...
   if not clockButtonUpdateTooltipHooked and name == "Blizzard_TimeManager" then
-  
+
+    -- The Blizzard UI only updates the TimeManagerClockButton (and with it the tooltip) once per second.
+    -- But we want the tooltip to be shown immediately!
+    TimeManagerClockButton:HookScript("OnEnter", function()
+      TimeManagerClockButton_UpdateTooltip()
+    end)
+
+
     -- Add PlayedTime to clock button tooltip!
     hooksecurefunc("TimeManagerClockButton_UpdateTooltip", function ()
 
@@ -15,27 +22,28 @@ timeManagerFrame:SetScript("OnEvent", function(self, event, ...)
       GameTooltip:AddLine(" ")
 
       brokerPlayedTime.OnTooltipShow(GameTooltip)
-     
+
+      -- Adjust tooltip size.
       GameTooltip:Show()
     end)
-    
+
     -- Add PlayedTime options to right click!
     TimeManagerClockButton:SetScript("OnClick", function (self, button)
-    
+
       GameTooltip:Hide()
-    
+
       if button == "RightButton" then
         brokerPlayedTime.OnClick(self, button)
         if TimeManagerFrame:IsVisible() then
           GameTooltip:Hide()
-        end   
+        end
       else
         TimeManagerClockButton_OnClick(self)
         if not TimeManagerFrame:IsVisible() then
           self:GetScript("OnEnter")(self)
-        end        
+        end
       end
-      
+
     end)
 
     clockButtonUpdateTooltipHooked = true
